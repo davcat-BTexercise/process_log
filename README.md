d8d15daa94b13dc1bf8233250e90501c8c31506f
David Catanho <david@catanho.com>
2016-07-07 20:53:40 +1000

    First commit: adding python script, resulting db file and log and README files

---
+++ README.md
@@ -0,0 +1,20 @@
+Exercise from BT
+
+Description
+
+Attached is a log file, it has 1,000 entries in it. The format of the entries of the log file differ depending on the event type:
+1. CVE event: Date, Time, Severity, Event ID, Hostname, Protocol, CVE ID.
+2. Access event: Date, Time, Severity Event ID, Source Address, Destination Address, User.
+ 
+With this file do the following:
+1. Write a python script that reads the log file and writes it into an sqlite database table.
+2. Verify that all entries are in the sqlite database.
+3. Tell us how many critical CVE issues are there?
+
+Please send to us:
+1. Git repository containing the Python code.
+2. The resulting sqlite database.
+
+Example:
+2016-05-27 22:01:46.403912 high 7564 host778.example.com TCP CVE-2015-6778
+2016-05-27 22:08:20.403929 high 9081 247.134.158.200 79.87.184.11 bryan
---
+++ process_log_file.py
@@ -0,0 +1,69 @@
+import sqlite3
+import sys
+
+
+def create_tables(database):
+    print("Trying to create tables")
+    try:
+        database.execute('''CREATE TABLE CVEEvents (date text,
+                                                    time text,
+                                                    severity text,
+                                                    event_id integer,
+                                                    hostname text,
+                                                    protocol text,
+                                                    cve_id text)''')
+
+        database.execute('''CREATE TABLE AccessEvents (date text,
+                                                       time text,
+                                                       severity text,
+                                                       event_id integer,
+                                                       source_address text,
+                                                       destination_address text,
+                                                       user text)''')
+        database.execute('''CREATE TABLE UnknownEvents (unknown_log_entry text)''')
+    except sqlite3.OperationalError:
+        print("Tables already exist. Continuing...")
+
+def clean_up_before_processing(cursor):
+    print("Deleting data from tables")
+    cursor.execute("DELETE FROM CVEEvents")
+    cursor.execute("DELETE FROM AccessEvents")
+    cursor.execute("DELETE FROM UnknownEvents")
+
+def file_processor(cursor):
+    print("Processing log file")
+    with open('log', 'r') as log_file: 
+        for line in log_file:
+            list_from_line = line.split()
+            if len(list_from_line) == 7:
+                first_3_characters_of_last_field = list_from_line[-1][0:3]
+                if first_3_characters_of_last_field == "CVE":
+                    cursor.execute("INSERT INTO CVEEvents VALUES (?,?,?,?,?,?,?)", tuple(list_from_line))
+                else:
+                    cursor.execute("INSERT INTO AccessEvents VALUES (?,?,?,?,?,?,?)", tuple(list_from_line))
+            elif len(list_from_line) > 0: 
+                cursor.execute("INSERT INTO UnknownEvents VALUES (?)", (line,))
+
+def count_critical_CVEs(cursor):
+    cursor.execute('''SELECT COUNT(*) FROM CVEEvents
+                      WHERE severity IN ('critical', 'crit', 'CRIT')''')
+    print("\nTotal number of critical CVEs: {}".format(cursor.fetchone()[0]))
+
+def main():
+    try:
+        assert sys.version_info >= (3,0)
+    except AssertionError:
+        print("This program runs in Python 3 or higher")
+        sys.exit(1)
+
+    log_database = sqlite3.connect('log.db')
+    log_cursor = log_database.cursor()
+    create_tables(log_database)
+    clean_up_before_processing(log_cursor)
+    file_processor(log_cursor)
+    count_critical_CVEs(log_cursor)
+    log_database.commit()
+    log_database.close()
+
+if __name__ == "__main__":
+    main()
